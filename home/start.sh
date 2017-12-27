952249491cfd6cd20f08ec2358f777997148c511
Vitaliy Filippov <vitalif@yourcmc.ru>
2017-12-27 20:50:49 +0300

    Add bugzilla job daemon

--- home/start.sh
+++ home/start.sh
@@ -5,6 +5,7 @@ export LC_ALL=ru_RU.UTF-8 LANG=ru_RU.UTF-8
 service sphinxsearch start
 service mysql start
 service bugzilla start
+service bugzilla-jobs start
 
 # Keep container running
 tail -fn0 /dev/null
